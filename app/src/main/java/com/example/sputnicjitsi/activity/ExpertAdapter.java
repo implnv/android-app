f590192e7495dbab5ed5b8f4551a0591be1cc0a9
Xenia V <k.volkova@sputnic.ru>
2023-08-15 09:44:49 +0500

    Диалоговое окно

--- app/src/main/java/com/example/sputnicjitsi/activity/ExpertAdapter.java
+++ app/src/main/java/com/example/sputnicjitsi/activity/ExpertAdapter.java
@@ -1,6 +1,7 @@
 package com.example.sputnicjitsi.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,24 +12,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.sputnicjitsi.CallActivity;
 import com.example.sputnicjitsi.R;
-import com.example.sputnicjitsi.WebSocket;
 import com.example.sputnicjitsi.experts.AvailableExperts;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import org.jitsi.meet.sdk.JitsiMeetActivity;
-import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
-import java.util.Objects;
-
-import io.socket.emitter.Emitter;
 
 public class ExpertAdapter extends RecyclerView.Adapter<ExpertAdapter.ViewHolder>{
     private final LayoutInflater inflater;
@@ -79,43 +66,12 @@ public class ExpertAdapter extends RecyclerView.Adapter<ExpertAdapter.ViewHolder
 
             Button button = view.findViewById(R.id.callButton);
             button.setOnClickListener(v -> {
-                try {
-                    WebSocket.getInstance().emit("call expert", new JSONObject().put("callId", Integer.parseInt(button.getText().toString())));
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
-
-                try {
-                    URL serverURL = new URL("https://pseudogu.ru/");
-
-                    WebSocket.getInstance().on("set room", new Emitter.Listener() {
-                        @Override
-                        public void call(Object... args) {
-                            JitsiMeetConferenceOptions options  = new JitsiMeetConferenceOptions.Builder()
-                                    .setServerURL(serverURL)
-                                    .setFeatureFlag("welcomepage.enabled", false)
-                                    .setFeatureFlag("toolbox.enabled", false)
-                                    .setFeatureFlag("filmstrip.enabled", false)
-                                    .setFeatureFlag("chat.enabled", false)
-                                    .setFeatureFlag("lobby-mode.enabled", false)
-                                    .setFeatureFlag("help.enabled", false)
-                                    .setFeatureFlag("overflow-menu.enabled", false)
-                                    .setFeatureFlag("prejoinpage.enabled", false)
-                                    .setFeatureFlag("pip.enabled", false)
-                                    .setFeatureFlag("pip-while-screen-sharing.enabled", false)
-                                    .setRoom(args[0].toString())
-                                    .build();
-
-                            JitsiMeetActivity.launch(v.getContext(), options);
-                        }
-                    });
-
-                } catch (MalformedURLException e) {
-                    throw new RuntimeException(e);
-                }
+                Intent intent = new Intent(v.getContext(), CallActivity.class);
+                intent.putExtra("num", button.getText().toString());
+                intent.putExtra("fio", nameView.getText());
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                view.getContext().startActivity(intent);
             });
         }
     }
-
-
 }
